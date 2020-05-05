702548b4276bbc7689e348dda9ae10a1e7083d74
Rebecca Herron <rebecca.c.herron@gmail.com>
2020-05-04 23:00:34 -0500

    adding mapbox to our angular project and creating some basic components like a drawer and title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tree-classifier-mapper';
+  menuOpen: any = true;
+
+  toggleMenu = () => {
+    this.menuOpen = !this.menuOpen;
+  }
+
 }
