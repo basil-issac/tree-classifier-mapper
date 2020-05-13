e9f725b3d59c0cd85edf9fcbd32a915ae5100a00
brianobmalay <nikadas157@comcast.net>
2020-05-13 14:25:25 -0500

    hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,17 +5,17 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "<REPLACE>",
+    apiKey: "AIzaSyBqnG0ew8C0kAwbavTKY8y5iO7yfO3H398",
     authDomain: "tree-classifier-mapper.firebaseapp.com",
     databaseURL: "https://tree-classifier-mapper.firebaseio.com",
     projectId: "tree-classifier-mapper",
     storageBucket: "tree-classifier-mapper.appspot.com",
-    messagingSenderId: "<REPLACE>",
-    appId: "<REPLACE>",
-    measurementId: "<REPLACE>"
+    messagingSenderId: "505260830076",
+    appId: "1:505260830076:web:c348cc5ec5f80de136c8f9",
+    measurementId: "G-4FK2P38WET"
   },
   mapbox: {
-    accessToken: "<REPLACE>"
+    accessToken: 'pk.eyJ1IjoiZ3JlYXRibHVlaGVycm9uIiwiYSI6ImNrOWMxNmgxbjAwY3gzbG40emlzbDh6eWkifQ.ehwxbzBHBp42izV7XNAzLQ'
   }
 };
 
